40d1dd5d974a2fbffeb91b78d63bf7c4642edc88
nammlt <124854398+nammlt@users.noreply.github.com>
2024-03-13 13:34:35 +0700

    Create NewMovie.js

---
+++ NewMovie.js
@@ -0,0 +1,7 @@
+[
+    { "id": "1", "title": "Dune Part Two", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-1.jpg" , "date" : "29 Feb 2024", "category1" : "Action", "category2" : "Adventure" },
+    { "id": "2", "title": "Kang Fu Panda 4", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-2.jpg" , "date" : "07 Mar 2024", "category1" : "Action", "category2" : "Adventure"},
+    { "id": "3", "title": "Poor Things", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-3.jpg" , "date" : "22 Feb 2024", "category1" : "Comedy", "category2" : "Drama"},
+    { "id": "4", "title": "The Zone of Interest", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-4.jpg" , "date" : "07 Mar 2024", "catagory1" : "Drama", "category2" : ""},
+    { "id": "5", "title": "The Bricklayer", "uri": "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-5.jpg" , "date" : "07 Mar 2024", "catagory1" : "Crime", "category2" : "Drama"}
+]
